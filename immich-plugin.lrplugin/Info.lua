b0d282e15d2a4f27939daead7f73eb445ca44fb1
Jacob Tian <39249179+ilikecandy@users.noreply.github.com>
2026-09-06 17:43:19 -0400

    feat: merge menus and bump to 4.4.0
    - keep upstream Reset Immich IDs plus fork Sync & Clean Deletions (both menus)
    - version 4.4.0 (ahead of upstream 4.3.2)

--- immich-plugin.lrplugin/Info.lua
+++ immich-plugin.lrplugin/Info.lua
@@ -36,6 +36,10 @@ return {
             file = "ResetImmichIds.lua",
             enabledWhen = "photosSelected",
         },
+        {
+            title = "Sync & Clean Deletions",
+            file = "SyncDeletionsDialog.lua",
+        },
     },
 
     LrExportMenuItems = {
@@ -47,11 +51,15 @@ return {
             title = "Immich import configuration",
             file = "ImportConfiguration.lua",
         },
+        {
+            title = "Sync & Clean Deletions",
+            file = "SyncDeletionsDialog.lua",
+        },
     },
 
     LrPluginInfoProvider = "PluginInfo.lua",
 
     LrPluginInfoURL = "https://github.com/bmachek/lrc-immich-plugin/",
 
-    VERSION = { major = 4, minor = 3, revision = 2, build = 0 },
+    VERSION = { major = 4, minor = 4, revision = 0, build = 0 },
 }
